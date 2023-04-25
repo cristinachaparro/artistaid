6476e595f5730ae86d9d87630993883f5b4bc121
Cristina Chaparro <cricbau@gmail.com>
2023-04-25 16:23:14 +0200

    improved footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
 function Footer() {
   return (
-    <div className="bg-slate-800 text-yellow-500 text-center text-xs">
-      <p>
-        Designed and developed by{" "}
-        <a href="https://github.com/cristinachaparro">Cristina Chaparro</a>.
-      </p>
-    </div>
+    <>
+      <div className="bg-slate-800 text-yellow-500 text-center text-xs">
+        <p>Designed and developed by Cristina Chaparro.</p>
+        <div className="flex justify-center">
+          <a href="https://github.com/cristinachaparro">
+            <img src="/public/icons8-github.svg" alt="GitHub icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/cristinachaparrobautista/">
+            <img src="/public/icons8-linkedin.svg" alt="LinkedIn icon" />
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 
